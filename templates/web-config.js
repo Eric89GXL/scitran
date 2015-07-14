d73342a6e6a4c7941f264c51fb0ee3f253fc3598
Nathaniel Kofalt <nathaniel@kofalt.com>
2015-07-14 15:59:14 -0500

    Follow config.toml layout in web-config.js

--- templates/web-config.js
+++ templates/web-config.js
@@ -3,8 +3,16 @@
 // Be sure you are editing the copy in templates/.
 //
 config = {
-	API_PREFIX: "{{browser.api_prefix}}",
-	DEMO_MODE: {{site.demo}},
-	CACHE_EXP: {{browser.cache_expire}},
-	MAX_RENDERED_SESSIONS: {{browser.render_limit}},
+	flywheel: {
+		version: {{flywheel.version}},
+	},
+	site: {
+		demo: {{site.demo}},
+		insecure: {{site.insecure}},
+	},
+	browser: {
+		api_prefix: "{{browser.api_prefix}}",
+		cache_expire: {{browser.cache_expire}},
+		render_limit: {{browser.render_limit}},
+	},
 };
